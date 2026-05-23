1188e7ec10b784f9794c9b4363f147c328771392
Abdulrahmanmoin <iam.armoin@gmail.com>
2026-05-23 22:18:17 +0500

    feat: add langchain-groq and langchain-tavily dependencies and implement basic tool-enabled chatbot agent

---
+++ 1_basic_chatbot/chatbot_with_tools.py
@@ -0,0 +1,77 @@
+# CHATBOT WITH TOOL
+
+import os
+from dotenv import load_dotenv
+from langchain_tavily import TavilySearch
+from langchain.chat_models import init_chat_model
+load_dotenv()
+
+
+llm = init_chat_model("groq:llama-3.1-8b-instant")
+
+tool=TavilySearch(max_results=3)
+# result=tool.invoke("What is langgraph?")
+# print(result)
+
+#custom functions
+
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+
+tools=[tool, multiply]
+
+llm_with_tools=llm.bind_tools(tools)
+
+llm_with_tools
+
+### StateGraph
+
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode
+from langgraph.prebuilt import tools_condition
+from langgraph.checkpoint.memory import MemorySaver
+
+memory=MemorySaver()
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+# Node definition
+def tool_calling_llm(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+# Graph
+
+builder = StateGraph(State)
+builder.add_node("tool_calling_llm", tool_calling_llm)
+builder.add_node("tools", ToolNode(tools))
+
+# Add edges
+builder.add_edge(START, "tool_calling_llm")
+builder.add_conditional_edges(
+    "tool_calling_llm",
+    tools_condition
+)
+builder.add_edge("tools", "tool_calling_llm")
+
+#compile the graph
+graph=builder.compile(checkpointer=memory)
+config={"configurable": {"thread_id": "chat1"}}
+response=graph.invoke({"messages": "Hi, my name is Abdul Rahman Moin."}, config)
+response=graph.invoke({"messages": "What is my name?"}, config)
+
+# print("[RESPONSE]: ", response)
+# print("response[messages][-1].content: ", response["messages"][-1].content)
+
+for m in response["messages"]:
+    print(m.pretty_print())
--- main.py
+++ main.py
@@ -2,11 +2,6 @@ def main():
     print("Hello from agentic-langgraph!")
 
 
-from typing import Annotated
-from typing_extensions import TypedDict
-from langgraph.graph import StateGraph, START, END
-from langgraph.graph.message import add_messages
-
 
 
 
